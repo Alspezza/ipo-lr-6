7d7201332ad75a8678ccd18475b565be2cfabf3b
alpsezza <zspecian23@gmail.com>
2025-10-12 16:54:39 +0300

    1

--- task_2/main.py
+++ task_2/main.py
@@ -1,26 +1,26 @@
 #Вариант 1 Специан Захар
 import random
-stroki = random.randint(4,8)
+#вводим библиотеку рандом и делаем рандомное количество строк и стольбцов от 4 до 8
+stroki = random.randint(4,8)     
 stolbci = random.randint(4,8)
-
-summa = 0
+#вводим некоторые пустые переменные и создаем переменную со списком рандомных чисел
 list1 =  [-3, -5, -2, -12, 0, 15, 4, 7, 2]
+summa = 0
 matrix = []
-row = []
+#цикл в цикле с помощью которого сначала заполняется строка числами из  list1
+#а после этого строка добавляется в матрицу
 for x in range(stroki):
-    row = []
+    strok = []
     for i in range(stolbci):
-        row.append(random.choice(list1))
-    matrix.append(row)
-
+        strok.append(random.choice(list1))
+    matrix.append(strok)
+#цикл который проходится по каждому значению из матрицы
 for row in matrix:
     for num in row:
+#проверка на четность и сложение числа если оно оказывается таковым
+        if (num % 2 == 0):
+            summa += num
+#выведение матрицы на экран в отформатированном виде и сумма 
         print(f"{num:3d}", end=" ")
     print()
-print("Количество строк, столбцов: ", stroki, " | ", stolbci)
-
-for strok in matrix:
-    for chislo in strok:
-        if (chislo % 2 == 0):
-            summa += chislo
 print ("Сумма всех четных чисел в матрице: ", summa)
