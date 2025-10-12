79b8c42c65b4ddb1a1a0754b9044efc0a7773d58
alpsezza <zspecian23@gmail.com>
2025-10-12 16:42:38 +0300

    1

--- task_2/main.py
+++ task_2/main.py
@@ -3,11 +3,24 @@ import random
 stroki = random.randint(4,8)
 stolbci = random.randint(4,8)
 
+summa = 0
 list1 =  [-3, -5, -2, -12, 0, 15, 4, 7, 2]
 matrix = []
-for strok in range(stroki):
-    for stolb in range(stolbci):
-        matrix.append(random.choice(list1))
-for stroki in matrix:
-    for num in stroki:
+row = []
+for x in range(stroki):
+    row = []
+    for i in range(stolbci):
+        row.append(random.choice(list1))
+    matrix.append(row)
+
+for row in matrix:
+    for num in row:
         print(f"{num:3d}", end=" ")
+    print()
+print("Количество строк, столбцов: ", stroki, " | ", stolbci)
+
+for strok in matrix:
+    for chislo in strok:
+        if (chislo % 2 == 0):
+            summa += chislo
+print ("Сумма всех четных чисел в матрице: ", summa)
